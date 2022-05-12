8b6184bddc059fa47dfd1b56766d1974f4e067cd
Jared Grippe <jared@deadlyicon.com>
2022-05-12 16:35:51 -0700

    converted src/main/*.ts to javascript

---
+++ src/main/preload.js
@@ -0,0 +1,19 @@
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron'
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    sendMessage(channel, args) {
+      ipcRenderer.send(channel, args)
+    },
+    on(channel, func) {
+      const subscription = (_event, ...args) =>
+        func(...args)
+      ipcRenderer.on(channel, subscription)
+
+      return () => ipcRenderer.removeListener(channel, subscription)
+    },
+    once(channel, func) {
+      ipcRenderer.once(channel, (_event, ...args) => func(...args))
+    },
+  },
+})
