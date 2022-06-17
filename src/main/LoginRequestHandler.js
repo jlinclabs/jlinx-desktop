e3d2667df230563189c1a5f1db7d67358610d7fb
Jared Grippe <jared@deadlyicon.com>
2022-06-17 13:31:04 -0700

    WIP

--- src/main/LoginRequestHandler.js
+++ src/main/LoginRequestHandler.js
@@ -26,32 +26,26 @@ module.exports = class LoginRequestHandler {
     debug('WATCHING FOR LOGIN REQUEST ON', appAccountId)
     const appAccount = await this.appAccounts.get(appAccountId)
     debug({ appAccount })
-    const appUserDoc = await this.jlinx.get(appAccount.appUserId)
-    const promise = appUserDoc.waitForUpdate()
+    const appUser = await this.jlinx.get(appAccount.appUserId)
+    const promise = appUser.waitForUpdate()
       .then(
         async () => {
-          debug('appUserDoc updated', appUserDoc)
-          await appUserDoc.update()
-          const [header, ...events] = await appUserDoc.allJson()
-          debug('appUserDoc header', header)
-          debug('appUserDoc events', events)
-          const newEvents = events
-            .map((event, index) => {
-              return {...event, id: `${appUserDoc.id}/${index}`}
-            })
-            .filter(event => !this._seen.has(event.id))
+          debug('appUser updated', appUser)
+          await appUser.update()
+          // const [header, ...events] = await appUser.allJson()
+          const sessionRequests = await appUser.getSessionRequests()
+          debug('appUser sessionRequests', sessionRequests)
+          const newSessionRequests = sessionRequests
+            .filter(sr => !this._seen.has(sr.id))
             .reverse()
-          for (const event of newEvents){
-            if (event && event.event === 'login-requested'){
-              // const age = new Date() - new Date(event.at)
-              // TODO check age
-              this._seen.add(event.id)
-              this._onLoginRequest({
-                ...event,
-                host: appAccount.host,
-              })
-              break
-            }
+
+          for (const sessionRequest of newSessionRequests){
+            this._seen.add(sessionRequest.id)
+            this._onLoginRequest({
+              ...sessionRequest,
+              host: appAccount.host,
+            })
+            break
           }
         },
         error => {
@@ -69,7 +63,7 @@ module.exports = class LoginRequestHandler {
   async get(id){
     const [docId, index] = id.split('/')
     // const appAccount = await this.appAccounts.get(appAccountId)
-    const appUserDoc = await this.jlinx.get(docId)
-    return await appUserDoc.getJson(index)
+    const appUser = await this.jlinx.get(docId)
+    return await appUser.getJson(index)
   }
 }
